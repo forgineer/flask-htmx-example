ddd10dbaf99eb4714fa1083a85a5c49080b57552
Blake Phillips <23227382+forgineer@users.noreply.github.com>
2024-11-27 00:17:46 -0600

    Updates to include HTMX and swaps for Register, Login, and Posts

--- src/HtmxBlog/blog.py
+++ src/HtmxBlog/blog.py
@@ -1,23 +1,42 @@
+import sqlite3
+
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
 from werkzeug.exceptions import abort
 
-from flaskr.auth import login_required
-from flaskr.db import get_db
+from .auth import login_required
+from .db import get_db
 
 bp = Blueprint('blog', __name__)
 
 
-@bp.route('/')
-def index():
-    db = get_db()
-    posts = db.execute(
+def get_all_posts() -> list:
+    """
+    Get ALL blog posts from the database.
+
+    :return: A list object with all blog posts data.
+    """
+    db: sqlite3.Connection = get_db()
+    all_posts: list = db.execute(
         'SELECT p.id, title, body, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
+
+    return all_posts
+
+
+@bp.route('/')
+def index():
+    posts: list = get_all_posts()
     return render_template('blog/index.html', posts=posts)
+
+
+@bp.route('/posts')
+def posts():
+    posts: list = get_all_posts()
+    return render_template('blog/posts.html', posts=posts)
 
 
 @bp.route('/create', methods=('GET', 'POST'))
--- src/HtmxBlog/db.py
+++ src/HtmxBlog/db.py
@@ -31,7 +31,9 @@ def init_db():
 
 @click.command('init-db')
 def init_db_command():
-    """Clear the existing data and create new tables."""
+    """
+    Clear the existing data and create new tables.
+    """
     init_db()
     click.echo('Initialized the database.')
 
